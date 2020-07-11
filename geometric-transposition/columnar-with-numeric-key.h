a4e3e30e049eb124b632307b72d03539f9d74bf7
David Gomesh <davidgomesh2016@gmail.com>
2020-07-10 22:51:46 -0300

    Columnar with numeric key

---
+++ geometric-transposition/columnar-with-numeric-key.h
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdio.h>
+
+void columnarWithNumericKey(char text[], char key[]){
+    int lengthText = strlen(text), lengthKey = strlen(key);
+    int positions[lengthKey];
+    char auxText[lengthText];
+
+    int linesCount, columnsCount = lengthKey;
+
+    for(int upper=65, lower=97, value=1; upper<=90, lower<=122; upper++, lower++){
+        for(int i=0; i<lengthKey; i++){
+            if(key[i]==upper || key[i]==lower){
+                positions[i] = value++;
+            }
+        }
+    }
+
+    if(lengthText % columnsCount == 0){
+        linesCount = lengthText / columnsCount;
+    }else{
+        linesCount = lengthText / columnsCount + 1;
+    }
+
+    char matrix[linesCount][columnsCount];
+
+    for(int i=0, k=0; i<linesCount; i++){
+        for(int j=0; j<columnsCount; j++){
+            if(k < lengthText){
+                matrix[i][j] = text[k++];
+            }else{
+                matrix[i][j] = '-';
+            }
+        }
+    }
+
+    int l=0;
+    for(int value=1; value<=lengthKey; value++){
+        for(int k=0; k<lengthKey; k++){
+            if(positions[k]==value){
+                for(int i=0, j=k; i<linesCount; i++){
+                    if(matrix[i][j] != '-'){
+                        auxText[l++] = matrix[i][j];
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    auxText[l] = '\0';
+    
+    strcpy(text, auxText);
+}
---
+++ geometric-transposition/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "process-strings.h"
+#include "simple-columnar.h"
+#include "simple-linear.h"
+#include "columnar-with-numeric-key.h"
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
